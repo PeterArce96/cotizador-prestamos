3faea7a345ce25625a0c9edcc1b39f32b561a091
PeterArce96 <parce0496@gmail.com>
2023-01-10 18:12:34 -0500

    cotizador - como modificar el State en React

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,17 @@
 // PROPS - Comunicación entre componentes
 // STATE - cual es el estado de tu app, carrito lleno o vacío, usuario autenticado,etc
 // escontramos los state antes del return, ejemplo: cantidad y la función que lo va a modificar es setCantidad, se recomienda ponerle "set" antes del nombre del state para la función
+// la función handleChange, el "handle" es por convención, se le coloca antes luego va el Evento que va a usar, en este caso el onChange.
 
 import { useState } from 'react'
 import Header from "./components/Header"
 
 function App() {
-  const [cantidad, setCantidad] = useState(10000);
+  let [cantidad, setCantidad] = useState(10000);
 
+  function handleChange(e) {
+    console.log(Number(e.target.value));
+  }
 
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
@@ -20,6 +24,7 @@ function App() {
       <input 
         type="range" 
         className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+        onChange={ handleChange }
       /> 
     </div>
   )
