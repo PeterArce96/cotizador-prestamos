a5e0b29afdd152c7bc9a8c658e42c58a86b5a777
PeterArce96 <parce0496@gmail.com>
2023-01-10 17:48:14 -0500

    cotizador - componentes en React

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 // en .jsx se puede combinar JS y HTML
 // código entre llaves "{}" lo va a tratar como JS
+  // en la función App y antes del return, puede escribir cualquier código de JS, for, funciones, variables
 
+import Header from "./components/Header"
 
 function App() {
 
-  const hola = "Hola Mundo";
-  // en la función App y antes del return, puede escribir cualquier código de JS, for, funciones, variables
-  const auth = false
-
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
-      {hola}
-
-      {auth ? 'autenticado': ' no autenticado'}
+      <Header />
     </div>
   )
 }
