289f7f6db2628b75a566cf8178351718d2a46458
PeterArce96 <parce0496@gmail.com>
2023-01-10 17:52:30 -0500

    cotizador - añadiendo input para las cantidades

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // en .jsx se puede combinar JS y HTML
 // código entre llaves "{}" lo va a tratar como JS
-  // en la función App y antes del return, puede escribir cualquier código de JS, for, funciones, variables
+// en la función App y antes del return, puede escribir cualquier código de JS, for, funciones, variables
+// etiquetas html que solo tengan apertura, se tienen que cerrar con "/>" al final
 
 import Header from "./components/Header"
 
@@ -9,6 +10,11 @@ function App() {
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header />
+
+      <input 
+        type="range" 
+        className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+      /> 
     </div>
   )
 }
