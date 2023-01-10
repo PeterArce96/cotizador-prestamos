4e887534cecbbb2d85cb1ba87d527eeec3c7bf56
PeterArce96 <parce0496@gmail.com>
2023-01-10 17:40:29 -0500

    cotizador - que es JSX?

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+// en .jsx se puede combinar JS y HTML
+// código entre llaves "{}" lo va a tratar como JS
+
 
 function App() {
 
+  const hola = "Hola Mundo";
+  // en la función App y antes del return, puede escribir cualquier código de JS, for, funciones, variables
+  const auth = false
+
   return (
-    <h1 className="text-6xl font-extrabold">Hola Mundo</h1>
+    <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
+      {hola}
+
+      {auth ? 'autenticado': ' no autenticado'}
+    </div>
   )
 }
 
